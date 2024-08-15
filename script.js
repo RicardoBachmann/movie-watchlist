017abc5a84b9800f1f1825ad1e25c510fc467f6a
RicardoBachmann <ricardo.bachmann@icloud.com>
2024-08-15 09:29:35 +0200

    Get API Data with async-await function and handle possible errors through data fetching

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=ff2c7e65`)
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+async function getMovieData() {
+  try {
+    const response = await fetch(
+      `http://www.omdbapi.com/?i=tt3896198&apikey=ff2c7e65`
+    );
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    const data = await response.json();
+    console.log(data);
+    let movieHtml = `
+            <h1>${data.Title}</h1>
+            <p>${data.Plot}</p>
+        `;
+    document.getElementById("container").innerHTML = movieHtml;
+  } catch (error) {
+    alert(error);
+  }
+}
+
+getMovieData();
